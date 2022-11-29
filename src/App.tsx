198fe50ff26eae45775f73ab01be4bbc04406b14
Tom Tillistrand <tomtillistrand@gmail.com>
2022-11-28 21:43:30 -0500

    Update default component styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,9 @@
 function App() {
-   return <h1 className="text-3xl font-bold underline">Hello World</h1>;
+  return (
+    <div className="h-full grid place-content-center">
+      <h1 className="text-3xl font-bold underline">Hello, World</h1>
+    </div>
+  );
 }
 
 export default App;
